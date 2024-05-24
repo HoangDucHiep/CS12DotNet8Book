d6c058cf2dd7225c553084803bc57822b12ae616
HoangDucHiep <Hunghiephainhuan1412@gmail.com>
2024-05-24 15:55:08 +0700

    Chapter 2

---
+++ Chapter02/Formatting/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberOfApples = 12;
+decimal priceOfApple = 0.35M;
+
+WriteLine("{0} apples cost {1:C}", numberOfApples, priceOfApple * numberOfApples);
+
+string formatted = string.Format("{0} apples cost {1:C}", numberOfApples, priceOfApple * numberOfApples);
+WriteLine(formatted);
